1842f0d8b365851bbe8237ba31c2300eab01f770
Ignacio Grane <graneignacio@gmail.com>
2024-08-14 13:25:47 -0300

    modificaciones pequeñas

--- src/components/Tabla.jsx
+++ src/components/Tabla.jsx
@@ -3,29 +3,31 @@ import TablaFila from "./TablaFila";
 const Tabla = ({ users, eliminarUsuario, setUsuarioAEditar }) => {
   return (
     <>
-      <h2 className="mt-3">Tabla de Usuarios</h2>
-      <hr />
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Edad</th>
-            <th>Color Favorito</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, idx) => (
-            <TablaFila
-              key={idx}
-              usuario={user}
-              eliminarUsuario={eliminarUsuario}
-              setUsuarioAEditar={setUsuarioAEditar}
-            />
-          ))}
-        </tbody>
-      </table>
+      <div className="mb-4">
+        <h2 className="mt-3">Tabla de Usuarios</h2>
+        <hr />
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Edad</th>
+              <th>Color Favorito</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, idx) => (
+              <TablaFila
+                key={idx}
+                usuario={user}
+                eliminarUsuario={eliminarUsuario}
+                setUsuarioAEditar={setUsuarioAEditar}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
